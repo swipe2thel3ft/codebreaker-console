33c785faf650f2f4af42e73301bfd06c80d184fe
Victor Perez <victorperez099@yahoo.com>
2021-06-16 12:04:50 -0600

    Created Guess model class.

---
+++ src/main/java/edu/cnm/deepdive/model/Guess.java
@@ -0,0 +1,74 @@
+package edu.cnm.deepdive.model;
+
+import com.google.gson.annotations.Expose;
+import java.util.Date;
+
+public class Guess {
+
+  @Expose
+  private String id;
+
+  @Expose
+  private Date created;
+
+  @Expose
+  private String text;
+
+  @Expose
+  private int exactMatches;
+
+  @Expose
+  private int nearMatched;
+
+  @Expose
+  private boolean solution;
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public Date getCreated() {
+    return created;
+  }
+
+  public void setCreated(Date created) {
+    this.created = created;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public int getExactMatches() {
+    return exactMatches;
+  }
+
+  public void setExactMatches(int exactMatches) {
+    this.exactMatches = exactMatches;
+  }
+
+  public int getNearMatched() {
+    return nearMatched;
+  }
+
+  public void setNearMatched(int nearMatched) {
+    this.nearMatched = nearMatched;
+  }
+
+  public boolean isSolution() {
+    return solution;
+  }
+
+  public void setSolution(boolean solution) {
+    this.solution = solution;
+  }
+
+}
