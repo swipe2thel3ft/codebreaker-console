1702bb76db5773d6cc172785bc64ea0e180f04a7
Victor Perez <victorperez099@yahoo.com>
2021-06-16 10:28:32 -0600

    Made game repository.

--- src/main/java/edu/cnm/deepdive/service/CodebreakerServiceProxy.java
+++ src/main/java/edu/cnm/deepdive/service/CodebreakerServiceProxy.java
@@ -3,6 +3,9 @@ package edu.cnm.deepdive.service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.model.Game;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
+import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Call;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -26,9 +29,15 @@ public interface CodebreakerServiceProxy {
           .excludeFieldsWithoutExposeAnnotation()
           .setDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
           .create();
+      HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+      interceptor.setLevel(Level.BODY);
+      OkHttpClient client = new OkHttpClient.Builder()
+          .addInterceptor(interceptor)
+          .build();
       Retrofit retrofit = new Retrofit.Builder()
           .baseUrl("https://ddc-java.services/codebreaker/")
           .addConverterFactory(GsonConverterFactory.create(gson))
+          .client(client)
           .build();
       INSTANCE = retrofit.create(CodebreakerServiceProxy.class);
 
---
+++ src/main/java/edu/cnm/deepdive/service/GameRepository.java
@@ -0,0 +1,29 @@
+package edu.cnm.deepdive.service;
+
+import edu.cnm.deepdive.model.Game;
+import java.io.IOException;
+import retrofit2.Response;
+
+public class GameRepository {
+
+  private final CodebreakerServiceProxy proxy;
+
+
+  public GameRepository() {
+    proxy = CodebreakerServiceProxy.getInstance();
+  }
+
+  public Game newGame(String pool, int length) throws IOException {
+    Game gameStub = new Game();
+    gameStub.setPool(pool);
+    gameStub.setLength(length);
+    Response<Game> response = proxy.startGame(gameStub).execute();
+    if (!response.isSuccessful()) {
+      throw new IllegalArgumentException();
+    }
+    return response.body();
+  }
+
+}
+
+
